e0184f45b3751913a43027f9161717c2a4386c5a
linhhut <linhhn@vega.com.vn>
2017-09-28 10:29:03 +0700

    add defaut login page

--- src/main/java/edu/hanoi/controller/HomeController.java
+++ src/main/java/edu/hanoi/controller/HomeController.java
@@ -3,9 +3,6 @@ package edu.hanoi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
-import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @Controller
 public class HomeController {
@@ -15,6 +12,14 @@ public class HomeController {
 		System.out.println("----- hello hanoi java clazz");
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("message", "Hello Java Clazz");
+		return mv;
+	}
+	
+	@RequestMapping(value="/nguoi-dung")
+	public ModelAndView forUser() {
+		System.out.println("----- for user");
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("message", "This is a protected page");
 		return mv;
 	}
 }
