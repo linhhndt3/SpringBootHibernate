39f5c239bff1d6fea3720a9dd222809330519323
linhhut <linhhn@vega.com.vn>
2017-09-28 09:12:58 +0700

    init project

---
+++ src/main/java/edu/hanoi/jazz/springjazz/SpringJazzApplication.java
@@ -0,0 +1,12 @@
+package edu.hanoi.jazz.springjazz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJazzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJazzApplication.class, args);
+	}
+}
